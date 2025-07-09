41593898a0a30fde01899603c31240702a92219d
Justin Jarae <lawnjarae@gmail.com>
2025-07-09 09:28:17 -0600

    Uses packer image rather than jammy

--- aws_instance.tf
+++ aws_instance.tf
@@ -1,9 +1,9 @@
-data "aws_ami" "ubuntu" {
+data "aws_ami" "vault_agent" {
   most_recent = true
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+    values = ["vault-agent-java-app-*"]
   }
 
   filter {
@@ -11,7 +11,7 @@ data "aws_ami" "ubuntu" {
     values = ["hvm"]
   }
 
-  owners = ["099720109477"] # Canonical
+  owners = ["self"]
 }
 
 # Get the required info from Vault
@@ -38,7 +38,7 @@ resource "vault_approle_auth_backend_role_secret_id" "brownfield_secret_id" {
 # End Vault
 
 resource "aws_instance" "instance" {
-  ami                         = data.aws_ami.ubuntu.id
+  ami                         = data.aws_ami.vault_agent.id
   instance_type               = "t3.micro"
   key_name                    = aws_key_pair.key_pair.key_name
   associate_public_ip_address = true
@@ -58,17 +58,17 @@ resource "null_resource" "configure_and_run_demo" {
   #   build_number = timestamp()
   # }
 
-  provisioner "file" {
-    source      = "apps/"
-    destination = "/home/ubuntu/"
+  # provisioner "file" {
+  #   source      = "apps/agent"
+  #   destination = "/home/ubuntu/"
 
-    connection {
-      type        = "ssh"
-      user        = "ubuntu"
-      private_key = tls_private_key.key.private_key_pem
-      host        = aws_eip.public_ip.public_ip
-    }
-  }
+  #   connection {
+  #     type        = "ssh"
+  #     user        = "ubuntu"
+  #     private_key = tls_private_key.key.private_key_pem
+  #     host        = aws_eip.public_ip.public_ip
+  #   }
+  # }
 
   provisioner "remote-exec" {
     inline = [
@@ -80,10 +80,6 @@ resource "null_resource" "configure_and_run_demo" {
       "echo ${data.vault_approle_auth_backend_role_id.brownfield_role_id.role_id} > role-id.txt",
       "echo ${vault_approle_auth_backend_role_secret_id.brownfield_secret_id.secret_id} > secret-id.txt",
       "echo ${vault_approle_auth_backend_role_secret_id.brownfield_secret_id.secret_id} > secret-id.txt.bak",
-      "sudo apt update",
-      "cd /home/ubuntu/startup-scripts",
-      "chmod +x install.sh",
-      "sudo ./install.sh",
       "sudo systemctl start brownfield-app"
     ]
 
--- configure-vault/vault.tf
+++ configure-vault/vault.tf
@@ -69,6 +69,22 @@ path "secret/data/brownfield-app-secrets" {
 path "postgres/creds/dev1" {
   capabilities = ["read"]
 }
+
+# Enable secrets engine
+path "sys/mounts/*" {
+  capabilities = [ "create", "read", "update", "delete", "list" ]
+}
+
+# List enabled secrets engine
+path "sys/mounts" {
+  capabilities = [ "read", "list" ]
+}
+
+# Work with pki secrets engine
+path "pki*" {
+  capabilities = [ "create", "read", "update", "delete", "list", "sudo", "patch" ]
+}
+
 EOT
 }
 
